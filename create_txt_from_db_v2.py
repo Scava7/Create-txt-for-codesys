b1f162020cdc678129b8f2cf8c6907e0d972b1a2
Luca Salad <144734060+Scava7@users.noreply.github.com>
2025-04-01 14:59:35 +0200

    Aggiunti spazi corretti e ;

--- create_txt_from_db_v2.py
+++ create_txt_from_db_v2.py
@@ -15,7 +15,7 @@ def scegli_file():
         filetypes=[("Database SQLite", "*.db *.sqlite3"),("Database SQLite", "*.db *.sqlite") , ("Tutti i file", "*.*")]
     )
 
-def prepara_blocco(df, header_title, device):
+def sts_blocks(df, header_title, device):
     
     # Group
     df["group_fmt"] = df["group"].apply(lambda g: f"(*{g}*)")   # Prepara le colonne formattate
@@ -25,8 +25,10 @@ def prepara_blocco(df, header_title, device):
     max_len_exch = df["exchange_variable"].str.len().max()    # Calcolo lunghezza cella per tabulare
 
     # Global variable name
-    df["global_variable_fmt"] = df["global_variable"].apply(lambda g: f"{g};")  # Prepara le colonne formattate
-    max_len_glob = df["global_variable_fmt"].str.len().max()                    # Calcolo lunghezza cella per tabulare
+    max_len_glob = df["global_variable"].str.len().max()                    # Calcolo lunghezza cella per tabulare
+
+    # Spazi di compensazione per global_variable
+    df["padding_glob"] = df["global_variable"].apply(lambda g: " " * (max_len_glob - len(g)))
     
     # Measurment unit
     if "measurment_unit" in df.columns:
@@ -49,15 +51,17 @@ def prepara_blocco(df, header_title, device):
 
     # Formattazione righe
     def formatta_riga(row):
-        group = row['group_fmt'].ljust(max_len_group + 1)
-        exch = row['exchange_variable'].ljust(max_len_exch + 1)
-        glob = row['global_variable_fmt'].ljust(max_len_glob + 1)
-        unit = row['measurment_unit_fmt'].ljust(max_len_unit + 1)
-        desc = row['description']
+        group       = row['group_fmt'].ljust(max_len_group)
+        exch        = row['exchange_variable'].ljust(max_len_exch)
+        glob        = row['global_variable']
+        glob_spaces = row['padding_glob']
+        unit        = row['measurment_unit_fmt'].ljust(max_len_unit)
+        desc        = row['description']
+
         if device == PLC:
-            return f"{group}{exch}:= {glob}//{unit}{desc}"
+            return f"{group} {exch} := {glob};{glob_spaces} //{unit} {desc}"
         else:
-            return f"{group}{glob}:= {exch}//{unit}{desc}"
+            return f"{group} {glob_spaces}{glob} := {exch}; //{unit} {desc}"
 
     righe = df.apply(formatta_riga, axis=1).tolist()
 
@@ -69,7 +73,7 @@ def genera_file_txt(db_path, tabelle, device):
 
     for nome_tabella, header_label in tabelle:
         df = pd.read_sql_query(f"SELECT * FROM {nome_tabella}", conn)
-        blocco = prepara_blocco(df, header_label, device)
+        blocco = sts_blocks(df, header_label, device)
         contenuto.extend(blocco)
 
     conn.close()
